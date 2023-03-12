2dcea494c9d9c5275129c39091448eb4a88778cc
Tom Dwyer <tom.uwe@gmail.com>
2023-03-12 21:41:59 +0000

    First RPC call

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+Service to grab public IP and post to smart contract - can be encrypted if desired for security.
+
+Ref: https://www.gakonst.com/ethers-rs/getting-started/start_a_new_project.html
+
+Polygon endpoint API info
+https://docs.polygonscan.com/getting-started/endpoint-urls
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+use dotenv::dotenv;
 use ethers::prelude::*;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    let rpc_url = std::env::var("RPC_WS").expect("RPC_WS must be set.");
+
+    let provider =  Provider::<Ws>::connect(rpc_url).await?;
+    let block_number: U64 = provider.get_block_number().await?;
+    println!("{block_number}");
+
+    Ok(())
 }
